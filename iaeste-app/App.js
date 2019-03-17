0136cb4a7ee12fcb99cf8d3cd6764ef40e4fad71
Anton Levholm <anton.badminton@gmail.com>
2019-03-17 20:37:34 +0100

    Add tab bar icons

--- iaeste-app/App.js
+++ iaeste-app/App.js
@@ -5,9 +5,12 @@ import {
   createAppContainer,
   createBottomTabNavigator
 } from "react-navigation";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 import Internships from "./pages/Internships";
 import InternshipDetails from "./pages/InternshipDetails";
+
+import IconWithBadge from "./components/IconWithBadge";
 
 class DetailsScreen extends React.Component {
   render() {
@@ -65,10 +68,37 @@ const DetailsStack = createStackNavigator(
   }
 );
 
-const Tabs = createBottomTabNavigator({
-  Internships: HomeStack,
-  Bookmarked: DetailsStack
-});
+const Tabs = createBottomTabNavigator(
+  {
+    Internships: HomeStack,
+    Bookmarked: DetailsStack
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        const IconComponent =
+          routeName === "Bookmarked" ? IconWithBadge : Ionicons;
+        const iconNames = {
+          Internships: "ios-globe",
+          Bookmarked: "ios-bookmark"
+        };
+        return (
+          <IconComponent
+            name={iconNames[routeName]}
+            size={25}
+            color={tintColor}
+            badgeCount={3}
+          />
+        );
+      }
+    }),
+    tabBarOptions: {
+      activeTintColor: "tomato",
+      inactiveTintColor: "gray"
+    }
+  }
+);
 
 const AppContainer = createAppContainer(Tabs);
 
